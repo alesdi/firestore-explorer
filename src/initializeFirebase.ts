71a5ea00eadde61f0b2555d81dd7ebea6f25c634
Alessandro Diano <alessandro.diano@me.com>
2022-04-09 23:20:26 +0200

    Add comments and refactor initializeFirebase

--- src/initializeFirebase.ts
+++ src/initializeFirebase.ts
@@ -8,36 +8,40 @@ import openServiceAccountSettings from "./openServiceAccountSettings";
 export async function initializeFirebase(
   context?: vscode.ExtensionContext,
 ): Promise<boolean> {
-  const filePath = (await vscode.workspace
-    .getConfiguration("firestore-explorer")
-    .get("serviceAccountKeyPath")) as string;
-
   try {
-    if (filePath === "") {
-      vscode.window
-        .showWarningMessage(
-          "You need to specify the path to your Firebase Service Account Key. [How to get my Service Account Key?](https://firebase.google.com/docs/admin/setup#initialize_the_sdk)",
-          "Open configuration",
-        )
-        .then(async (string) => {
-          if (string === "Open configuration") {
-            await openServiceAccountSettings();
-          }
-        });
+    // If Firebase is not initialized yet, attempt to do it now
+    if (admin.apps.length === 0 || admin.apps[0]?.firestore() === undefined) {
+      // First obtain the service account key path
+      const filePath = (await vscode.workspace
+        .getConfiguration("firestore-explorer")
+        .get("serviceAccountKeyPath")) as string;
 
-      throw new Error("Not specified");
-    }
+      // If no path is provided, throw an exception and advise to fill in the configuration
+      if (filePath === "") {
+        vscode.window
+          .showWarningMessage(
+            "You need to specify the path to your Firebase Service Account Key. [How to get my Service Account Key?](https://firebase.google.com/docs/admin/setup#initialize_the_sdk)",
+            "Open configuration",
+          )
+          .then(async (string) => {
+            if (string === "Open configuration") {
+              await openServiceAccountSettings();
+            }
+          });
 
-    if (admin.apps.length === 0) {
-      const app = admin.initializeApp({
+        throw new Error("Service account key path not specified");
+      }
+
+      // Initialize the Firebase app with the service account key
+      admin.initializeApp({
         credential: admin.credential.cert(require(filePath)),
       });
     }
-
+    // Firebase is initialized.
     return true;
   } catch (e) {
-    console.log(e);
+    // Something went wrong. Firebase could not be initialized.
+    console.error(e);
+    return false;
   }
-
-  return false;
 }
