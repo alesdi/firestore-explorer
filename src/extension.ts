b0cf0c52e300acd30cbe048c4d1cb7dbd1f52b42
Alessandro Diano <alessandro.diano@me.com>
2022-04-10 23:13:52 +0200

    Fix show more items command

--- src/extension.ts
+++ src/extension.ts
@@ -57,7 +57,7 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand(
       "firestore-explorer.showMoreItems",
-      () => explorerDataProvider.refresh()
+      (path: string) => explorerDataProvider.showMoreItems(path)
     )
   );
   context.subscriptions.push(explorerView);
